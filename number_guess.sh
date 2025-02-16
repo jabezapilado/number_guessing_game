a214a6f529f551f0ec374c607443de44c0144dca
Jabez Timothy Apilado <jabzstickz@gmail.com>
2025-02-16 16:55:58 +0000

    test: final code

--- number_guess.sh
+++ number_guess.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-# INPUT_NAME() {
-#   echo "Enter your username:"
-#   read NAME
-#   n=${#NAME}
+INPUT_NAME() {
+  echo "Enter your username:"
+  read NAME
+  n=${#NAME}
 
-  #When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input.Your database should allow usernames that are 22 characters
+  
   if [[ ! $n -le 22 ]] || [[ ! $n -gt 0 ]]
   then
     INPUT_NAME
@@ -14,23 +14,16 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
     USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE username='$NAME';") | sed 's/ //g')
     if [[ ! -z $USER_NAME ]]
     then
-      #If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., 
-      #with <username> being a users name from the database,
-      #<games_played> being the total number of games that user has played, 
-      #and <best_game> being the fewest number of guesses it took that user to win the game
       USER_ID=$(echo $($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME';") | sed 's/ //g')
       USER_NAME=$(echo $($PSQL "SELECT username FROM users WHERE user_id='$USER_ID';") | sed 's/ //g')
       GAME_PLAYED=$(echo $($PSQL "SELECT frequent_games FROM users WHERE user_id=$USER_ID;") | sed 's/ //g')
       BEST_GAME=$(echo $($PSQL "SELECT MIN(best_guess) FROM users LEFT JOIN games USING(user_id) WHERE user_id=$USER_ID;") | sed 's/ //g')
       echo "Welcome back, $USER_NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
     else
-      #If the username has not been used before, you should print Welcome, <username>! It looks like this is your first time here.
       USER_NAME=$NAME
       echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
     fi
 
-    #Your script should randomly generate a number that users have to guess
-    #The next line printed should be Guess the secret number between 1 and 1000: and input from the user should be read
     CORRECT_ANSWER=$(( $RANDOM % 1000 + 1 ))
     GUESS_COUNT=0
     INPUT_GUESS $USER_NAME $CORRECT_ANSWER $GUESS_COUNT
@@ -48,7 +41,6 @@ INPUT_GUESS() {
     echo "Guess the secret number between 1 and 1000:"
     read USSER_GUESS
   else
-    #If anything other than an integer is input as a guess, it should print That is not an integer, guess again:
     echo "That is not an integer, guess again:"
     read USSER_GUESS
   fi
@@ -68,11 +60,6 @@ CHECK_ANSWER() {
   GUESS_COUNT=$3
   USSER_GUESS=$4
   
-  #Until they guess the secret number, 
-  #it should print 
-  #It's lower than that, guess again: if the previous input was higher than the secret number, and
-  #It's higher than that, guess again: if the previous input was lower than the secret number. 
-  #Asking for input each time until they input the secret number.
   if [[ $USSER_GUESS -lt $CORRECT_ANSWER ]]
   then
     echo "It's lower than that, guess again:"
